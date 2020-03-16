2360b44c342288cdd15dfab5400bbb4bff1fdbeb
Lucas Santos Dev <lucas_ss.arts@live.com>
2020-03-16 17:34:24 -0300

    chore: add new configurations about export components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import ButtonChangeTheme from './ButtonChangeTheme';
+import CardPicture from './CardPicture';
+import Copyright from './Copyright';
+import DetailsCard from './DetailsCard';
+import Footer from './Footer';
+import Jumbotron from './Jumbotron';
+import ModalNavbar from './ModalNavbar';
+import Navbar from './Navbar';
+
+export const ButtonChangeTheme;
+export const CardPicture;
+export const Copyright;
+export const DetailsCard;
+export const Footer;
+export const Jumbotron;
+export const ModalNavbar;
+export const Navbar;
